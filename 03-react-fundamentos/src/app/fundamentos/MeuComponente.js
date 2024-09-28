724090a8c00d2f3f9dec977adfe8dd313d809e33
ThiagoTK2 <thiago.r.silva@iesb.edu.br>
2024-09-27 22:33:05 -0300

    Sera?

---
+++ 03-react-fundamentos/src/app/fundamentos/MeuComponente.js
@@ -0,0 +1,9 @@
+export default function MeuComponente () {
+
+    return (
+        <>
+            <h2>Meu Componente</h2>
+            <p>O texto do meu componente</p>
+        </>
+    )
+}
---
+++ 03-react-fundamentos/src/app/fundamentos/page.js
@@ -0,0 +1,18 @@
+import MeuComponente from "./MeuComponente";
+
+export default function Fundamentos () {
+    
+    const teste = "teste"
+
+    function getNumero () {
+        return 2
+    }
+    
+    
+    
+    return (
+        <> 
+        <MeuComponente />
+        </>
+    )
+} 
